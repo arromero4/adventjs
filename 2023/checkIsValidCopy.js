698d597ba35cd748edc6ae836fd2a8f8314d5b39
arromero4 <arromero0408@gmail.com>
2023-12-13 00:04:46 -0600

    Challenge #12: Is it a valid copy?

---
+++ 2023/checkIsValidCopy.js
@@ -0,0 +1,67 @@
+// Reto #12: ¿Es una copia válida?
+// Challenge #12: Is it a valid copy?
+// En el Polo Norte todavía usan fotocopiadoras de papel. Los elfos las usan para copiar las cartas que los niños envían a Santa y así poder enviarlas a todos los departamentos de regalos.
+
+// Sin embargo ya son muy viejas y no funcionan muy bien. Cada vez que hacen una copia, la calidad de la copia disminuye ligeramente, un fenómeno conocido como pérdida generacional.
+
+// Necesitas detectar si una carta es una copia de otra. Las cartas son muy largas y no puedes leerlas, pero puedes compararlas con un algoritmo.
+
+// Existe una gran probabilidad de que un caracter se degrade en cada copia (¡no pasa siempre!). Y al ocurrir, la regla que sigue es:
+
+// Los caracteres de la A a la Z se degradan de mayúsculas a minúsculas (A-Z ⇒ a-z)
+// Las letras se degradan en una serie de caracteres en este orden: a-z ⇒ # ⇒ + ⇒ : ⇒ . ⇒
+// Una vez degradadas las letras en los símbolos, se pueden continuar degradando.
+// Ten en cuenta que el último es un espacio en blanco, no un caracter vacío.
+// Los caracteres que no son letras (como los dígitos) no se degradan.
+// Sabiendo esto y recibiendo dos cartas. La supuesta original y la copia. Debes determinar si la copia es una copia de la otra.
+
+// checkIsValidCopy(
+//   'Santa Claus is coming',
+//   'sa#ta cl#us is comin#'
+// ) // true
+// checkIsValidCopy(
+//   'Santa Claus is coming',
+//   'p#nt: cla#s #s c+min#'
+// ) // false (por la p inicial)
+// checkIsValidCopy('Santa Claus', 's#+:. c:. s') // true
+// checkIsValidCopy('Santa Claus', 's#+:.#c:. s') // false (hay un # donde no debería)
+// Para entender cómo funcionan las fotocopiadoras y su degradación, mira este ejemplo:
+
+// original:  'Santa Claus'
+// 1ª copia:  'santa cla#s'
+// 2ª copia:  'sa#t# cl#+s'
+// 3ª copia:  'sa+## c#+:s'
+// 4ª copia:  's#++. c+:.s'
+// 5ª copia:  's#+:. c:. s'
+// Por lo tanto s#+:. c+:++ es una copia válida de Santa Claus. Y, como ves, la degradación de las letras no se produce en un orden específico, es aleatorio.
+
+// Basado en el desafío de CodeWars Photocopy decay
+
+function checkIsValidCopy(original, copy) {
+    let originalCopy = original.toLowerCase().split(' ');
+    let copyCopy = copy.toLowerCase().split(' ');
+
+    console.log(originalCopy, copyCopy);
+    // Expresión regular que busca los símbolos #, +, :, .
+var regex = /[#\+:.\s]/g;
+// Utilizar el método match para encontrar coincidencias
+var matches = copy.match(regex);
+
+// Verificar si se encontraron coincidencias
+if (matches) {
+    console.log("Se encontraron las siguientes coincidencias:", matches);
+} else {
+    console.log("No se encontraron coincidencias.");
+}
+  }
+
+// checkIsValidCopy(
+//   'Santa Claus is coming',
+//   'sa#ta cl#us is comin#'
+// ) // true
+// checkIsValidCopy(
+//   'Santa Claus is coming',
+//   'p#nt: cla#s #s c+min#'
+// ) // false (por la p inicial)
+checkIsValidCopy('Santa Claus', 's#+:. c:. s') // true
+checkIsValidCopy('Santa Claus', 's#+:.#c:. s') // false (hay un # donde no debería)
