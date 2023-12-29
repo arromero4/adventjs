79c2b82ff55313c25b1bd2f34683967b1439b08b
arromero4 <arromero0408@gmail.com>
2023-12-29 01:18:51 -0600

    Challenge #23: Christmas dinner

---
+++ 2023/organizeChristmasDinner.js
@@ -0,0 +1,65 @@
+// Reto #23: 🍽️ La comida de Navidad
+// Challenge #23: 🍽️ Christmas dinner
+// ¡Santa 🎅 está organizando una gran cena navideña 🫓 y quiere asegurarse de que todos los platos sean únicos y variados!
+
+// Te da una lista de platos navideños donde cada elemento consiste en una lista de strings que comienza con el nombre del plato, seguido de todos los ingredientes utilizados para su preparación.
+
+// Tienes que escribir una función que agrupe los platos por ingredientes siempre que haya al menos 2 platos que los contengan.
+
+// Así que devolvemos un array de arrays donde la primera posición es el nombre del ingrediente y el resto los nombres de los platos.
+
+// Tanto la lista de ingredientes como los platos deben estar ordenados alfabéticamente.
+
+// const dishes = [
+//   ["christmas turkey", "turkey", "sauce", "herbs"],
+//   ["cake", "flour", "sugar", "egg"],
+//   ["hot chocolate", "chocolate", "milk", "sugar"],
+//   ["pizza", "sauce", "tomato", "cheese", "ham"],
+// ]
+
+// organizeChristmasDinner(dishes)
+
+// /*
+
+// "sauce" está en 2 platos: "christmas turkey" y "pizza".
+// "sugar" está en 2 platos: "cake" y "hot chocolate".
+// El resto de ingredientes solo aparecen en un plato, por lo que no los mostramos.
+
+// Enseñamos primero "sauce" porque alfabéticamente está antes que "sugar".
+// Y los platos de cada ingrediente también están ordenados alfabéticamente.
+
+// [
+//   ["sauce", "christmas turkey", "pizza"],
+//   ["sugar", "cake", "hot chocolate"]
+// ]
+// */
+// Ten en cuenta que:
+
+// Todos los nombres de los platos son diferentes.
+// Los nombres de los ingredientes para un plato dado son distintos entre sí.
+// Si no hay ingredientes repetidos, devolvemos un array vacío.
+function organizeChristmasDinner(dishes) {
+  const ingredients = {};
+
+  for (const [dish, ...dishIngredients] of dishes) {
+    for (const ingredient of dishIngredients) {
+      if (ingredients[ingredient]) {
+        ingredients[ingredient].push(dish);
+      } else {
+        ingredients[ingredient] = [dish];
+      }
+    }
+  }
+
+  const result = [];
+
+  for (const ingredient in ingredients) {
+    if (ingredients[ingredient].length > 1) {
+      result.push([ingredient, ...ingredients[ingredient].sort()]);
+    }
+  }
+
+  result.sort((a, b) => a[0].localeCompare(b[0]));
+
+  return result;
+}
