c17a8f244cad96201fe2aa087568a4ec80f36e68
arromero4 <arromero0408@gmail.com>
2024-01-04 02:54:55 -0600

    Challenge #25: Calculating distances

---
+++ 2023/travelDistance.js
@@ -0,0 +1,53 @@
+// Reto #25: 🗺️ Calculando distancias
+//Challenge #25: 🗺️ Calculating distances
+// Ya ha entregado Santa Claus 🎅 todos los regalos a los niños pero quieren revisar si pueden mejorar de cara al año que viene.
+
+// Los elfos quieren saber cuántos movimientos ha hecho Santa Claus 🛷 para entregar todos los regalos. Para ello, te dan un mapa de la ciudad con la ubicación de cada niño y de Santa.
+
+// El mapa es una cadena de texto multi línea donde cada caracter representa una casilla. Los niños se representan por números del 1 al 9 y Santa Claus por la letra S. El resto de casillas son .
+
+// Santa Claus sólo puede moverse hacia arriba, abajo, izquierda o derecha, y cada movimiento cuenta como 1 km. Además, siempre empieza en la posición S y debe entregar los regalos en orden, del 1 al 9.
+
+// const map = `.....1....
+// ..S.......
+// ..........
+// ....3.....
+// ......2...`
+
+// const result = travelDistance(map)
+// console.log(result) // -> 12 km
+// /*
+// De la S al niño 1: 4 movimientos
+// Del niño 1 al 2: 5 movimientos
+// Del niño 2 al 3: 3 movimientos
+// Total: 12 movimientos
+// */
+
+// const result2 = travelDistance(`..S.1...`)
+// console.log(result2) // -> 2
+// Escribe una función travelDistance que reciba un mapa y devuelva la distancia total que ha recorrido Santa Claus según la posición de los niños.
+
+// Ten en cuenta que:
+
+// El mapa no tiene por qué ser cuadrado.
+// El mapa siempre tendrá al menos un niño.
+// El mapa siempre tendrá una posición inicial para Santa Claus.
+// Los números de los niños nunca se repiten.
+
+function travelDistance(map) {
+    let m = map.split("\n")
+    let findPos = (s) => {
+      const y = m.findIndex (r => r.includes(s))
+      if(y != -1) return {y, x:m[y].indexOf(s)} 
+    }
+    let santaPos = findPos("S"), childPos
+    let i=1, kms =0
+    while(childPos=findPos(i)){
+     kms += Math.abs(santaPos.x - childPos.x)
+     kms += Math.abs(santaPos.y - childPos.y)
+  
+     santaPos = childPos
+     i++
+    }
+    return kms
+  }
